83e55e3b519d3b4ad4f341fff1a0b2d374b4add4
K R HARI PRAJWAL <hariprajwal77@gmail.com>
2025-11-05 14:40:05 +0530

    Create auto.bat

---
+++ auto.bat
@@ -0,0 +1,6 @@
+
+
+@echo off
+cd /d "C:\Users\harip\EXPERIMENT"
+"C:\Users\harip\AppData\Local\Programs\Python\Python311\Scripts\jupyter-nbconvert.exe" --to notebook --execute "A A A TELEGERAM FAST FINAL.ipynb" --inplace
+pause
